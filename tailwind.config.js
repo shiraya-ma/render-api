5db10ae9727d27d697164e94c7bc2973c6ea67e6
shiraya-ma <mai.shirayama@outlook.jp>
2024-08-30 16:08:18 +0900

    :art: NextUIのテーマを設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,41 @@
 'use strict';
 import { nextui } from "@nextui-org/react";
 
-const themeColor = {
-  primary: {
-    50:  "#ddefed",
-    100: "#aad8d2",
-    200: "#73bfb5",
-    300: "#3aa698",
-    400: "#009383",
-    500: "#00816f",
-    600: "#007563",
-    700: "#006655",
-    800: "#005647",
-    900: "#003b2c",
-    DEFUALT: "#3aa698",
-    foreground: '#fff'
-  }
-}
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: [
+    'class',
   ],
   theme: {
     extend: {},
   },
   plugins: [
     nextui({
-      extend: {
-        theme: {
-          dark: {
-            colors: themeColor
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#3aa698",
+              foreground: "#fff",
+            }
           },
-          light: {
-            colors: themeColor
-          }
-        }
-      }
-    })
+        },
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: "#3aa698",
+              foreground: "#fff",
+            },
+            background: '#1F2937'
+          },
+        },
+      },
+  })
   ],
 };
+
+module.exports = config;

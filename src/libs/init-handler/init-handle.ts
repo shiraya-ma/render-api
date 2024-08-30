0ac2d40f55188b50756305cf4fa6ac6015810a12
shiraya-ma <mai.shirayama@outlook.jp>
2024-08-30 11:53:07 +0900

    :pen: ハンドラのイニシャライザ実装

---
+++ src/libs/init-handler/init-handle.ts
@@ -0,0 +1,28 @@
+'use strict';
+import { NextApiHandler } from 'next';
+
+export function initHandler (option: InitHandlerOption) {
+    const defaultHandler: NextApiHandler = (req, res) => {
+        res.status(400).send(`
+            <h1>Bad Request</h1>
+
+            <p>Not support method [${ req.method }] on ${ req.url }</p>
+        `)
+    };
+
+    const handler: NextApiHandler = (req, res) => {
+        const _h: NextApiHandler = (option as any)[req.method!] ?? defaultHandler;
+
+        _h(req, res);
+    };
+
+    return handler;
+};
+
+export type InitHandlerOption = {
+    DELETE?:    NextApiHandler;
+    GET?:       NextApiHandler;
+    PATCH?:     NextApiHandler;
+    POST?:      NextApiHandler;
+    PUT?:       NextApiHandler;
+};

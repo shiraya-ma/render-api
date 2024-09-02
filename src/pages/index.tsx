04687db430789efa9b5e744064abfc5c0fbc4f19
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-02 17:41:33 +0900

    :pen: インデックスページをTS化

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+'use strict';
+import React from 'react';
+
+const Page: React.FC<Page.Props> = (props) => {
+    const {} = props;
+    
+    return (
+        <></>
+    );
+};
+
+namespace Page {
+    export type Props = {};
+};
+
+export default Page;

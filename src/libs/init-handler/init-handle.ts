1b0ca4e825a337e22d1b93cf981ae782d2ce32bf
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-03 15:17:46 +0900

    :bug: warning対応

--- src/libs/init-handler/init-handle.ts
+++ src/libs/init-handler/init-handle.ts
@@ -7,13 +7,13 @@ export function initHandler (option: InitHandlerOption) {
             <h1>Bad Request</h1>
 
             <p>Not support method [${ req.method }] on ${ req.url }</p>
-        `)
+        `);
     };
 
     const handler: NextApiHandler = (req, res) => {
         const _h: NextApiHandler = (option as any)[req.method!] ?? defaultHandler;
 
-        _h(req, res);
+        return _h(req, res);
     };
 
     return handler;

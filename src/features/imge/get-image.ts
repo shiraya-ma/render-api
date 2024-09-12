3550e8d44a1b73280258da48cfbb174a35ed05ad
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-12 15:01:07 +0900

    :pen: 画像を取得する関数を追加

---
+++ src/features/imge/get-image.ts
@@ -0,0 +1,66 @@
+'use strict';
+import { log } from '@/libs';
+
+const REG_CONTENT_TYPE = /^image\/.+/;
+
+export async function getImage (url: string): Promise<Image | undefined> {
+    log.info('[GET-IMAGE] url:', url);
+
+    try {
+        const res = await fetch(url, {
+            headers: {
+                // TwitterBotのUserAgen設定
+                "User-Agent": "Twitterbot/1.0 Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/5.12.3 Chrome/69.0.3497.128 Safari/537.36"
+            }
+        });
+    
+        if (!res.ok) {
+            log.error('[GET-IMAGE] Failed fetch to', url);
+
+            return undefined;
+        }
+
+        const contentLength = res.headers.get('content-length');
+
+        if (!contentLength) {
+            log.error('[GET-IMAGE] Not found content-length');
+
+            return undefined;
+        }
+
+        const blob = await res.blob();
+
+        const contentType = blob.type ?? res.headers.get('content-type');
+
+        if (!contentType) {
+            log.error('[GET-IMAGE] Not found content-type');
+
+            return undefined;
+        }
+
+
+        if (!REG_CONTENT_TYPE.test(contentType)) {
+            log.error('[GET-IMAGE] Not match content-type to image', contentType);
+
+            return undefined;
+        }
+
+        const buffer = new Uint8Array(await blob.arrayBuffer());
+
+        return {
+            contentLength,
+            contentType,
+            data: buffer
+        }
+    } catch (e) {
+        log.error(e);
+
+        return undefined;
+    }
+}
+
+type Image = {
+    contentType: string;
+    contentLength: string;
+    data: any
+};
